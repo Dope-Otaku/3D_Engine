2e7168cdbbd9aec9216d5e089f8fba48bd667abf
Dope-Otaku <fakeyt909@gmail.com>
2025-02-11 00:07:23 +0530

    updated the fps display variable

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class ObjectRenderer:
         pop.init() #called pygame for accessing the modules
         self.RES = self.WIDTH, self.HEIGHT = 1600, 900
         self.H_WIDTH, self.H_HEIGHT = self.WIDTH // 2, self.HEIGHT // 2
-        self.FPS = 60 #adjusting the fps 
+        self.FPS = 60 #adjusting the max fps 
         self.screen = pop.display.set_mode(self.RES)
         self.clock = pop.time.Clock()
 
@@ -17,9 +17,10 @@ class ObjectRenderer:
         self.screen.fill(pop.Color('darkslategray'))
 
     def run(self):
-        while true:
+        while True:
             self.draw()
             [exit() for i in pop.event.get() if i.type == pop.QUIT]
+            pop.display.set_caption(str(self.clock.get_fps()))
             pop.display.flip()
             self.clock.tick(self.FPS)
 
